1a66861638b926865788865cdd06989a2504d0f5
Alex Nascimento <alexjose68@hotmail.com>
2025-01-18 13:03:00 -0300

    feat: exemplo de Query Params e de Route Params

--- src/routes.ts
+++ src/routes.ts
@@ -3,9 +3,25 @@ import { Router, Request, Response } from "express"
 const router = Router();
 
 // Exemplo: http://localhost:3333/tarefas
-router.get("/tarefas", (req: Request, res: Response) => {
-  // res.send("Minha primeira API");
-  res.json({ message: "Minha primeira api!", aluno: "Alex Nascimento" });
+
+// Query Params ?nome=Comprar Pao
+// router.get("/tarefas", (req: Request, res: Response) => {
+//   const nome = req.query.nome;
+
+//   res.json({ tarefa: nome });
+// })
+
+
+// Route Params /tarefas/2
+router.get("/tarefas/:id", (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  res.json({ tarefa: `Tarefa com id: ${id}` });
 })
+
+
+
+// Request Body { nome: "Comprar Pao", "usuario": 123 }
+
 
 export { router };
